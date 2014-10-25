a717d75a245cd5f38061e0191c70d8dfc4ae7dd2
rshen1993 <rshen1993@gmail.com>
2014-10-25 17:41:42 -0400

    example for retrieving data from server

---
+++ featureService.js
@@ -0,0 +1,85 @@
+'use strict';
+
+angular.module('myApp', [])
+.controller('PlatformCtrl',
+function ($sce, $scope, $rootScope, $log, $window, $timeout, serverApiService, platformMessageService, stateService) {
+
+  /*var platformUrl = $window.location.search;
+  var gameUrl = platformUrl.length > 1 ? platformUrl.substring(1) : null;
+  if (gameUrl === null) {
+    $log.error("You must pass the game url like this: ...platform.html?<GAME_URL> , e.g., http://yoav-zibin.github.io/emulator/platform.html?http://yoav-zibin.github.io/TicTacToe/game.html");
+    $window.alert("You must pass the game url like this: ...platform.html?<GAME_URL> , e.g., ...platform.html?http://yoav-zibin.github.io/TicTacToe/game.html");
+    return;
+  }
+  $scope.gameUrl = $sce.trustAsResourceUrl(gameUrl);*/
+
+  var gameID = 5137355874762752;
+  serverApiService.sendMessage(
+      [{getGames: {}}],
+      function (response) {
+        $scope.games = angular.toJson(response, true);
+      });
+
+  /*while ($scope.games === undefined) {
+
+  }
+
+  console.log($scope.games);*/
+
+
+  function waitforserver() {
+    if ($scope.games === undefined) {
+      $timeout(function() {waitforserver();}, 200);
+    }else{
+      done();
+    }
+  }
+
+  waitforserver();
+
+  function done() {
+    console.log($scope.games);
+  }
+
+
+  var gotGameReady = false;
+  $scope.startNewMatch = function () {
+    stateService.startNewMatch();
+  };
+  $scope.getStatus = function () {
+    if (!gotGameReady) {
+      return "Waiting for 'gameReady' message from the game...";
+    }
+    var matchState = stateService.getMatchState();
+    if (matchState.endMatchScores) {
+      return "Match ended with scores: " + matchState.endMatchScores;
+    }
+    return "Match is ongoing! Turn of player index " + matchState.turnIndex;
+  };
+  $scope.playMode = "passAndPlay";
+  stateService.setPlayMode($scope.playMode);
+  $scope.$watch('playMode', function() {
+    stateService.setPlayMode($scope.playMode);
+  });
+
+  platformMessageService.addMessageListener(function (message) {
+    if (message.gameReady !== undefined) {
+      gotGameReady = true;
+      var game = message.gameReady;
+      game.isMoveOk = function (params) {
+        platformMessageService.sendMessage({isMoveOk: params});
+        return true;
+      };
+      game.updateUI = function (params) {
+        platformMessageService.sendMessage({updateUI: params});
+      };
+      stateService.setGame(game);
+    } else if (message.isMoveOkResult !== undefined) {
+      if (message.isMoveOkResult !== true) {
+        $window.alert("isMoveOk returned " + message.isMoveOkResult);
+      }
+    } else if (message.makeMove !== undefined) {
+      stateService.makeMove(message.makeMove);
+    }
+  });
+});
