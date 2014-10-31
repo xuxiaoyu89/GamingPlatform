92c3bb2e211c0719c216c1f0adcb8ffcde86d6ef
rshen1993 <rshen1993@gmail.com>
2014-10-31 14:50:24 -0400

    change refreshing time to 10secs

--- platform_game_vs.js
+++ platform_game_vs.js
@@ -169,8 +169,8 @@ serverApiService.sendMessage(
 //====================================================
 
 
-//Check changes periodically(every 2sec)
-var interval = setInterval(checkChanges, 2000);
+//Check changes periodically(every 10sec)
+var interval = setInterval(checkChanges, 10000);
 
 //function for updating match status and game UI
 function updateStatus() {
