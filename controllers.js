8dad08278ee9452f3d54634e7bb372a2c4bff7fc
Vangie Shue <vangieshue@gmail.com>
2014-10-31 16:05:06 -0400

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -1,17 +1,1 @@
-myAppControllers
-
-var myAppControllers = angular.module('phonecatControllers', []);
-
-phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('phones/phones.json').success(function(data) {
-      $scope.phones = data;
-    });
-
-    $scope.orderProp = 'age';
-  }]);
-
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.phoneId = $routeParams.phoneId;
-  }]);
+var myAppControllers = angular.module('myAppControllers', []);
