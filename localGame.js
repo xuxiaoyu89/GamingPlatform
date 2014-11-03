084443e3d709a28fb86a92aa26734d2adad1b866
xuxiaoyu89 <xuxiaoyu89@gmail.com>
2014-11-03 16:21:40 -0500

    Update localGame.js

--- localGame.js
+++ localGame.js
@@ -141,9 +141,52 @@ serverApiService.sendMessage(
     });
 //====================================================
 
+  var gotGameReady = false;
+  $scope.startNewMatch = function () {
+    stateService.startNewMatch();
+  };
+  $scope.getStatus = function () {
+    if (!gotGameReady) {
+      return "Waiting for 'gameReady' message from the game...";
+    }
+    var matchState = stateService.getMatchState();
+    if (matchState.endMatchScores) {
+      return "Match ended with scores: " + matchState.endMatchScores;
+    }
+    return "Match is ongoing! Turn of player index " + matchState.turnIndex;
+  };
+  $scope.playMode = "passAndPlay";
+  stateService.setPlayMode($scope.playMode);
+  $scope.$watch('playMode', function() {
+    stateService.setPlayMode($scope.playMode);
+  });
+
+  platformMessageService.addMessageListener(function (message) {
+    if (message.gameReady !== undefined) {
+      gotGameReady = true;
+      var game = message.gameReady;
+      game.isMoveOk = function (params) {
+        platformMessageService.sendMessage({isMoveOk: params});
+        return true;
+      };
+      game.updateUI = function (params) {
+        platformMessageService.sendMessage({updateUI: params});
+      };
+      stateService.setGame(game);
+    } else if (message.isMoveOkResult !== undefined) {
+      if (message.isMoveOkResult !== true) {
+        //$window.alert("isMoveOk returned " + message.isMoveOkResult);
+      }
+    } else if (message.makeMove !== undefined) {
+      stateService.makeMove(message.makeMove);
+    } else {
+      //$window.alert("Platform got: " + angular.toJson(message, true));
+    }
+  });
+
 
 //platform listen to iframe for player's moves
-
+/*
     var move;//move made by player
     platformMessageService.addMessageListener(function (message) {
         $log.info("PlatformMessageService: got a message.");
@@ -192,4 +235,5 @@ serverApiService.sendMessage(
         }
     });
 //====================================================
+*/
 });
