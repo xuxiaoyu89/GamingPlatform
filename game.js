178f61fcd3b244659f65445e9366dac2c736d9de
Vangie Shue <vangieshue@gmail.com>
2014-11-05 00:47:30 -0500

    Update game.js

--- game.js
+++ game.js
@@ -6,12 +6,12 @@ function ($sce, $scope, $rootScope, $log, $window, $routeParams, serverApiServic
   
   //==========HANDLES DIV RESIZING==================//
     // Cache the div so that the browser doesn't have to find it every time the window is resized.
-    var div2 = $('div.SMPG_game_goback');
+    //var div2 = $('div.SMPG_game_goback');
     // Run the following when the window is resized, and also trigger it once to begin with.
     //$(window).resize(function () {
       // Get the current height of the div and save it as a variable.
       //var height2 = div2.height();
-      $log.info("DIV 2: ", div2);
+      //$log.info("DIV 2: ", div2);
       // Set the font-size and line-height of the text within the div according to the current height.
       //$div.css({
       //'font-size': (height/2) + 'px',
