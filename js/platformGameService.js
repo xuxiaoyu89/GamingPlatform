a18a47bd1ca9b92d03454ff8530513db5c775ee6
rshen1993 <rshen1993@gmail.com>
2014-11-11 14:55:12 -0500

    Update platformGameService.js

--- js/platformGameService.js
+++ js/platformGameService.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'; 
 
 angular.module('myApp')
 .service('platformGameService', function($rootScope, $log, $window, $routeParams, $interval, serverApiService, platformMessageService) {
