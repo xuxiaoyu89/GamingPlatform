f65bce0d76b37669260c384c276d5284166cecfc
rshen1993 <rshen1993@gmail.com>
2014-11-11 15:43:19 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -14,7 +14,7 @@ function ($sce, $scope, $rootScope, $log, $window, $routeParams, platformGameSer
   $scope.player0 = platformGameService.getPlayer0;
   $scope.player1 = platformGameService.getPlayer1;
   $scope.gameStatus = platformGameService.getGameStatus;
-  //$scope.gameUrl = platformGameService.getTestUrl;
+  $scope.gameUrl = platformGameService.getGameUrl;
 
   //CONSTANT VARIABLES
   var MENU_URL = '#/menu';
@@ -72,7 +72,7 @@ function ($sce, $scope, $rootScope, $log, $window, $routeParams, platformGameSer
   
   //$scope.gameUrl = $sce.trustAsResourceUrl($scope.gameUrl);//game url to be used for showing the game in iframe
   
-  $scope.gameUrl = $sce.trustAsResourceUrl(platformGameService.getTestUrl());
+  $scope.gameUrl = $sce.trustAsResourceUrl($scope.gameUrl);
   
   platformGameService.beginLoop();
 });
