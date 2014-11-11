21212f49d23bbcc6d9a5995c3e121b961456402f
rshen1993 <rshen1993@gmail.com>
2014-11-11 15:52:26 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -67,11 +67,11 @@ function ($sce, $scope, $rootScope, $log, $window, $routeParams, platformGameSer
 
 
   platformGameService.fetchGameUrl();
-  $log.info("$scope.gameUrl:",$scope.gameUrl);
   
   
   platformGameService.fetchGameUrldev(function(url) {
     $log.info("game.js returned fetchGameUrldev: ", url)
+    $log.info("$scope.gameUrl:",$scope.gameUrl);
   });
   
   //$scope.gameUrl = $sce.trustAsResourceUrl($scope.gameUrl);//game url to be used for showing the game in iframe
