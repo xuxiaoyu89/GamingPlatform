7139f61fbe8e6acbcb0ed8d813e7fa4068d5a42b
xuxiaoyu89 <xuxiaoyu89@gmail.com>
2014-11-12 14:56:36 -0500

    Update localGameService.js

--- js/localGameService.js
+++ js/localGameService.js
@@ -1,1 +1,21 @@
 //create game service
+'use strict'; 
+
+angular.module('myApp')
+.service('localGameService', function($rootScope, $log, $window, $interval, stateService, platformMessageService) {
+
+  
+  this.clearInterval = function() {
+    if($rootScope.menu_interval !== undefined){
+      $interval.cancel($rootScope.menu_interval);
+    }
+  }
+
+  //===================== JS_ERROR_CATCHING ====================//
+  // Quick function to both alert and log requested message as error
+  function alert_log_error(alert, log) {
+    $log.error("Alert & Log Error: ", log);
+    return;
+  }
+
+});
