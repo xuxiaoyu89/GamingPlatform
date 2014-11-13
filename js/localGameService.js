93947ebb2d3654eeb0e171a3bf8a12606b7741a9
xuxiaoyu89 <xuxiaoyu89@gmail.com>
2014-11-13 10:53:44 -0500

    Update localGameService.js

--- js/localGameService.js
+++ js/localGameService.js
@@ -17,6 +17,43 @@ angular.module('myApp')
     $log.error("Alert & Log Error: ", log);
     return;
   }
+  
+  var MENU_URL = '#/menu';
+  var gameID;
+  var playMode;
+  
+  this.setLocalGame = function(entireUrl){
+    var hashedUrl = entireUrl.split('#')
+    var beforeHashUrl = hashedUrl[0];
+    $log.info("beforeHash URL: ", beforeHashUrl);
+    MENU_URL=beforeHashUrl.concat(MENU_URL);
+    $log.info("Menu URL: ", MENU_URL);
+    
+    platformUrl = hashedUrl[1].split('?');
+    $log.info("Platform URL: ", platformUrl);
+  
+    if (platformUrl.length <2) {
+        alert_log_error("URL Invalid.", "Required URL Format: ...#/game?matchid=1&gameid=2");
+    } else {
+      platformUrl2 = platformUrl[1];
+      $log.info("Platform URL2: ", platformUrl2);
+    }
+    //Split URL and pull params
+    var parsedurl = platformUrl2.split('&');
+    $log.info("Parsed URL: ", parsedurl);
+    var subparse;
+    var i;
+    for (i = 0; i < parsedurl.length; i++) {
+        subparse = parsedurl[i].split('=');
+        if (subparse.length === 2) {
+            if (subparse[0].toLowerCase() === 'playmode') {
+                playMode = subparse[1];
+            } else if (subparse[0].toLowerCase() === 'gameid') {
+                gameID = subparse[1];
+            }
+        }
+    }
+  }
 
   this.fetchGameUrl = function (callback) {
     $log.info("localGameService getGameUrl");
