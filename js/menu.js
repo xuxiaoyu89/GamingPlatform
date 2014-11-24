b4002c3c34b3d668fab798dbabc2dde993b1edf4
Vangie Shue <vangieshue@gmail.com>
2014-11-24 16:24:03 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -451,10 +451,17 @@ function ($sce, $scope, $rootScope, $log, $window, $timeout, $location, $interva
                                     var icon_match = $doc.getElementById('SMPG_icon_match');
                                     var icon_AI = $doc.getElementById('SMPG_icon_AI');
                                     var icon_stats = $doc.getElementById('SMPG_icon_stats');
-                                    icon_pass.style.fontSize = (height_menubottom - 22) + 'px';
-                                    icon_match.style.fontSize = (height_menubottom - 22) + 'px';
-                                    icon_AI.style.fontSize = (height_menubottom - 22) + 'px';
-                                    icon_stats.style.fontSize = (height_menubottom - 22) + 'px';
+                                    if(height_menubottom>=30) {
+                                        icon_pass.style.fontSize = (height_menubottom - 22) + 'px';
+                                        icon_match.style.fontSize = (height_menubottom - 22) + 'px';
+                                        icon_AI.style.fontSize = (height_menubottom - 22) + 'px';
+                                        icon_stats.style.fontSize = (height_menubottom - 22) + 'px';
+                                    } else {
+                                        icon_pass.style.fontSize = (height_menubottom - 6) + 'px';
+                                        icon_match.style.fontSize = (height_menubottom - 6) + 'px';
+                                        icon_AI.style.fontSize = (height_menubottom - 6) + 'px';
+                                        icon_stats.style.fontSize = (height_menubottom - 6) + 'px';
+                                    }
                                 }
     }
     rescaleDivs();
